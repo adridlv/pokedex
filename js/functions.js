0e65f65dc3fbe1594d7713aab6a2a4ab14bbae41
maraviflo <adriandlv53@gmail.com>
2016-06-21 15:25:25 +0200

    error bucle infinito tipo

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,6 @@
+var counter = 0;
+var i = 0;
+
 var Pokemon = function(name, types, weight){
   this.name = name,
   this.types = types,
@@ -17,15 +20,48 @@ function generateCard(pokemon){
   "<h2 class=\"namePokemon\">"+pokemon.name+"</h2>"+
   "<div class=\"types\" ><p>"+ type +"</p></div>"+
   "<div class=\"weight\" ><p>"+pokemon.weight +"</p></div>"+
-  "</div>"
+  "</div>";
 
   $(".container-pokemon").append(html);
 }
 
-function searchPokemon(index){
+function searchPokemon(search, searchControl){
   var name = "";
   var type= [];
   var weight="";
+  if(searchControl){
+    counter = 0;
+    i = 1;
+
+    console.log("buscando");
+    searchPokemonByType(i,search);
+    
+  }
+  else{
+    searchPokemonById(search);
+  }
+}
+
+function searchPokemonById(index){
+  $.ajax({
+   url: 'http://pokeapi.co/api/v2/pokemon/'+index+'/',
+   data: {
+    format: 'json'
+  },
+  error: function() {
+
+  },
+  dataType: 'json',
+  success: function(data){
+    var pokemon = new Pokemon(data.name,data.types,data.weight);
+    generateCard(pokemon); 
+  },
+  type: 'GET'
+
+});
+}
+
+function searchPokemonByType(index, type){
 
   $.ajax({
    url: 'http://pokeapi.co/api/v2/pokemon/'+index+'/',
@@ -33,26 +69,36 @@ function searchPokemon(index){
     format: 'json'
   },
   error: function() {
-    $('#info').html('<p>An error has occurred</p>');
+
   },
   dataType: 'json',
-  success: function(data) {
-   name = data.name;
-   type = data.types;
-   weight = data.weight;
-   var pokemon = new Pokemon(name,type,weight);
-   generateCard(pokemon); 
- },
- type: 'GET'
+  success: function(data){
+    var pokemon = new Pokemon(data.name,data.types,data.weight);
+
+    if(pokemon.types[0].type.name === type || pokemon.types[1].type.name === type){
+      console.log("entro por aqui");
+      generateCard(pokemon); 
+      counter++;
+      i++;
+    }
+    
+  },
+  type: 'GET'
 
 });
+  
 }
 
 $(document).ready(function(){
   $(".search").keypress(function(event){
     if(event.keyCode === 13){
       console.log("he entrado");
-      searchPokemon($(this).val());
+      var texto = $(this).val();
+      var searchControl = false;
+
+      //checkear que exista el tipo POR HACER
+      searchControl = isNaN(texto);
+      searchPokemon($(this).val(), searchControl);
     }
   });
 });
