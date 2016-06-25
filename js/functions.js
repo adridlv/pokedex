c421988645d2f5114236629d3c4a9cafc4b3f3aa
Adrian de la Viña <Adrian de la Via>
2016-06-25 21:43:23 +0200

    css added

--- js/functions.js
+++ js/functions.js
@@ -10,21 +10,23 @@ var Pokemon = function(name, types, weight, url){
 }
 
 function generateCard(pokemon){
-  var type = pokemon.types[0].type.name;
+  var type = pokemon.types[0].type.name[0].toUpperCase() + pokemon.types[0].type.name.slice(1);
   
   if(pokemon.types[1] != undefined)
-    type += " "+pokemon.types[1].type.name;
+    type += " "+pokemon.types[1].type.name[0].toUpperCase() + pokemon.types[1].type.name.slice(1);
   
   console.log(type);
 
   var html = 
   "<div class=\"card\">"+
-  "<img src=\""+pokemon.url+"\">"+
-  "<h2 class=\"namePokemon\">"+pokemon.name+"</h2>"+
-  "<div class=\"types\" ><p>"+ type +"</p></div>"+
-  "<div class=\"weight\" ><p>"+pokemon.weight +"</p></div>"+
-  "<div class=\"container-evolutions\">"+pokemon.evolution+"</div>"+
-  "</div>";
+  "<img class=\"imgPokemon\"src=\""+pokemon.url+"\">"+
+  "<h2 class=\"namePokemon\">"+pokemon.name[0].toUpperCase() + pokemon.name.slice(1)+"</h2>"+
+  "<div class=\"types\" ><p>Type: "+ type +"</p></div>"+
+  "<div class=\"weight\" ><p>Weight: "+pokemon.weight +"</p></div>"+
+  "<div class=\"back\"></div>"
+  "</div>"
+
+  ;
 
   $(".container-pokemon").append(html);
 }
